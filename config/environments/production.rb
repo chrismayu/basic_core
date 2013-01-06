34ffa93ce5545a5ecc3cb4175b238bc198f4b46e
Chris Mailloux <chrismayu@gmail.com>
2013-01-06 15:37:49 -0500

    Add SSL in production2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,7 @@ BasicCore::Application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, 
    # and use secure cookies.
    config.force_ssl = true
+   
   # Code is not reloaded between requests
   config.cache_classes = true
 
