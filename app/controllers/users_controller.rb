4a7ed4921fc9d74db6576d2d089fa179d23be060
Chris Mailloux <chrismayu@gmail.com>
2013-01-13 15:59:03 -0500

    done and up to 9.29

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
- # before_filter :signed_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
-#  before_filter :correct_user,   only: [:edit, :update]
+ before_filter :signed_in_user, only: [ :edit, :update] #:index, :edit, :update, :destroy, :following, :followers]
+  before_filter :correct_user,   only: [:edit, :update]
   #before_filter :admin_user,     only: :destroy
 
   def index
@@ -34,15 +34,14 @@ class UsersController < ApplicationController
   
 
   def update
-      @user = User.find(params[:id])
-      if @user.update_attributes(params[:user])
-        flash[:success] = "Profile updated"
-        sign_in @user
-        redirect_to @user
-      else
-        render 'edit'
-      end
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
     end
+  end
 
   def destroy
     User.find(params[:id]).destroy
@@ -66,12 +65,21 @@ class UsersController < ApplicationController
 
   private
 
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
-    end
+ 
 
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
+    
+    def signed_in_user
+          unless signed_in?
+            store_location
+            redirect_to signin_url, notice: "Please sign in."
+          end
+        end
+
+        def correct_user
+          @user = User.find(params[:id])
+          redirect_to(root_path) unless current_user?(@user)
+        end
 end
