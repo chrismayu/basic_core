465da19ee96640b710272abdaec225e5b41407f1
Chris Mailloux <chrismayu@gmail.com>
2013-01-13 18:33:52 -0500

    9.53

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -112,22 +112,36 @@ describe "Authentication" do
                end
         
         
-               describe "as non-admin user" do
-                     let(:user) { FactoryGirl.create(:user) }
-                     let(:non_admin) { FactoryGirl.create(:user) }
+        
+               describe "when attempting to visit a protected page" do
+                       before do
+                         visit edit_user_path(user)
+                         fill_in "Email",    with: user.email
+                         fill_in "Password", with: user.password
+                         click_button "Sign in"
+                       end
 
-                     before { sign_in non_admin }
+                       describe "after signing in" do
 
-                     describe "submitting a DELETE request to the Users#destroy action" do
-                       before { delete user_path(user) }
-                       specify { response.should redirect_to(root_path) }        
+                         it "should render the desired protected page" do
+                           page.should have_selector('title', text: 'Edit user')
+                         end
+
+                         describe "when signing in again" do
+                           before do
+                             delete signout_path
+                             visit signin_path
+                             fill_in "Email",    with: user.email
+                             fill_in "Password", with: user.password
+                             click_button "Sign in"
+                           end
+
+                           it "should render the default (profile) page" do
+                             page.should have_selector('title', text: user.name) 
+                           end
+                         end
+                       end
                      end
-                   end
-        
-        
-        
-        
-        
           end
   
   
