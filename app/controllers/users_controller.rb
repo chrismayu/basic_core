6ee394b9647cde179fc73a0b362519af7f1b89a5
Chris Mailloux <chrismayu@gmail.com>
2013-01-12 23:00:06 -0500

    done and up to 9.10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome - Are you ready to feel Twitterpated?"
+      flash[:success] = "Welcome - To the Basic Core App"
       redirect_to @user
     else
       render 'new'
@@ -28,19 +28,21 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
   
   
 
   def update
-    if @user.update_attributes(params[:user])
-      flash[:success] = "Profile updated"
-      sign_in @user
-      redirect_to @user
-    else
-      render 'edit'
+      @user = User.find(params[:id])
+      if @user.update_attributes(params[:user])
+        flash[:success] = "Profile updated"
+        sign_in @user
+        redirect_to @user
+      else
+        render 'edit'
+      end
     end
-  end
 
   def destroy
     User.find(params[:id]).destroy
